996998f1f1328ae4ddfb2c93e581c0e0f97e360b
nassima bression <benasima@yahoo.fr>
2024-09-11 14:51:23 +0200

    Feat[Header et Footer]: création du header et du footer

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./screens/home/home.component";
+import {ContactComponent} from "./screens/contact/contact.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent, title:'Accueil' },
+  { path: 'contact', component: ContactComponent, title:'Contact' }
+];
+

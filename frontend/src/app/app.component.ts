ffc4eb3f2a6dcaca5e15afbced28b31acb4e65d9
nassima bression <benasima@yahoo.fr>
2024-09-16 13:36:53 +0200

    Feat[header]: refactoring du style + responsive designe

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, NgClass],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ROMCARE';
+
+  constructor(private router: Router) {}
+
+  // ouvrir le menu de navigation en mobile
+  isMenuOpen = false;
+  // fermer le menu de navigation en mobile
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+// surligner les liens de navigation
+  isActive(route: string): boolean {
+    return this.router.url === route;
+  }
 }
+
+
--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,19 @@
+
 import { Routes } from '@angular/router';
-import {HomeComponent} from "./screens/home/home.component";
-import {ContactComponent} from "./screens/contact/contact.component";
+import { HomeComponent } from './screens/home/home.component';
+import { ContactComponent } from './screens/contact/contact.component';
+import {TarifsComponent} from "./screens/tarifs/tarifs.component";
+import {DermopigmentationComponent} from "./screens/dermopigmentation/dermopigmentation.component";
+import {FaqComponent} from "./screens/faq/faq.component";
+import {AProposComponent} from "./screens/a-propos/a-propos.component";
+
 
 export const routes: Routes = [
-  { path: 'home', component: HomeComponent, title:'Accueil' },
-  { path: 'contact', component: ContactComponent, title:'Contact' }
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Optionnel, redirige vers la page d'accueil par défaut
+  { path: 'home', component: HomeComponent, title: 'Accueil' },
+  { path: 'contact', component: ContactComponent, title: 'Contact' },
+  { path: 'a-propos', component: AProposComponent, title: 'A propos' },
+  { path: 'tarifs', component:TarifsComponent, title: 'Tarifs' },
+  { path: 'la-dermopigmentation-réparatrice', component: DermopigmentationComponent, title: 'La-dermopigmentation-réparatrice' },
+  { path: 'faq', component: FaqComponent, title: 'Faq' },
 ];
-

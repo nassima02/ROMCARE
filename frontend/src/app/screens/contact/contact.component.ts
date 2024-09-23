76bc460c2fdd443c0afef9a6402fa9f8800e28ac
nassima bression <benasima@yahoo.fr>
2024-09-23 11:19:08 +0200

    refactoring: ajout bannière et section à proximité à la page contact

--- frontend/src/app/screens/contact/contact.component.ts
+++ frontend/src/app/screens/contact/contact.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {ButtonComponent} from "../../shared/components/button/button.component";
+import {BannerComponent} from "../../shared/components/banner/banner.component";
 
 @Component({
   selector: 'app-contact',
   standalone: true,
     imports: [
-        ButtonComponent
+        ButtonComponent,
+        BannerComponent
     ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'

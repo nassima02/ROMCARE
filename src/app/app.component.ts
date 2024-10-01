72d5f60c462a5ebcb94eb8c43e5c90bc637e82b5
nassima bression <benasima@yahoo.fr>
2024-10-01 10:12:03 +0200

    refactoring: ajout dans le footer le lien vers linkedin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
 import { NgClass } from '@angular/common';
+import {TimelineModule} from "primeng/timeline";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, RouterLink, NgClass],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, RouterLink, NgClass, TimelineModule],
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ROMCARE';
+	title = "ROMCARE Cabinet d'Infirmier libéral spécialisé en dermopigmentation médicale d’aréoles mammaires";
+	isMenuOpen = false;
 
-  constructor(private router: Router) {}
+	constructor(private router: Router) {
+		// Écoutez les événements de navigation
+		this.router.events.subscribe((event) => {
+			if (event instanceof NavigationEnd) {
+				window.scrollTo(0, 0); // Défilement vers le haut de la page
+			}
+		});
+	}
 
-  // ouvrir le menu de navigation en mobile
-  isMenuOpen = false;
-  // fermer le menu de navigation en mobile
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
-  }
-// surligner les liens de navigation
-  isActive(route: string): boolean {
-    return this.router.url === route;
-  }
+	// Ouvrir le menu de navigation en mobile
+	toggleMenu() {
+		this.isMenuOpen = !this.isMenuOpen;
+	}
+
+	// Surligner les liens de navigation
+	isActive(route: string): boolean {
+		return this.router.url === route;
+	}
 }
+
 
 
